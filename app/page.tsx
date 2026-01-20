a9cbdc70feadfef248fba4d708c64e0b7a0f671f
3point14pi <lucaswcheng8@gmail.com>
2026-01-20 19:24:01 +0000

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,20 @@ export default function Home() {
     <div className={styles.page}>
       <h1 className={styles.title}>Lucas Projects</h1>
       <div className={styles.buttonContainer}>
-        <a href="https://pooppuncher.vercel.app">
-          <Image src="/pooppuncher.png" alt="Pooppuncher" width={450} height={225} />
-        </a>
-        <a href="https://deskpet.org">
-          <Image src="/deskpets.png" alt="Deskpets" width={400} height={225} />
-        </a>
-        <a href="https://ispyai.com">
-          <Image src="/ispyai.png" alt="iSpyAi" width={400} height={225} />
-        </a>
-        <br />
-        <a href="https://roachranger-hykucy6qjxgetw447gcsfx.streamlit.app/">
-          <Image src="/ispyai.png" alt="iSpyAi" width={400} height={225} />
-        </a>
+      <a href="https://pooppuncher.vercel.app">
+        <Image src="/pooppuncher.png" alt="Pooppuncher" width={450} height={225} />
+      </a>
+      <a href="https://deskpet.org">
+        <Image src="/deskpets.png" alt="Deskpets" width={400} height={225} />
+      </a>
+      <a href="https://ispyai.com">
+        <Image src="/ispyai.png" alt="iSpyAi" width={400} height={225} />
+      </a>
+      </div>
+      <div className={styles.buttonContainer}>
+      <a href="https://roachranger-hykucy6qjxgetw447gcsfx.streamlit.app/">
+        <Image src="/ispyai.png" alt="roachRanger" width={400} height={225} />
+      </a>
       </div>
     </div>
   );
